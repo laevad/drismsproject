927505bb284c7914e037aa5bc58393b5bcc7837d
laevad <djaldave@gmail.com>
2022-06-17 15:25:14 +0800

    fixed practical button

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,23 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Dashboard;
-use App\User; 
-use App\Image; 
+use App\User;
+use App\Image;
 use App\Notification;
- 
+
 use App\Course;
 use App\Fleet;
-use App\Branch; 
-use App\School;  
+use App\Branch;
+use App\School;
 use App\StudentCourse;
 use App\Classes;
 use App\Day;
-use App\SchoolCourse; 
+use App\SchoolCourse;
 use App\Evaluation;
- 
+
 
 use Illuminate\Http\Request;
-use Auth; 
+use Auth;
 use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
@@ -30,17 +30,17 @@ class DashboardController extends Controller
      *
      * @return void
      */
- 
+
     public function __construct()
     {
-        $this->middleware('auth');   
+        $this->middleware('auth');
         // $this->middleware(function ($request, $next) {
-        //     $this->user = Auth::user();  
+        //     $this->user = Auth::user();
         //     if($this->user->role != 'Admin'){
         //         return redirect()->back();
-        //     } 
+        //     }
         //     return $next($request);
-        // });  
+        // });
     }
 
     /**
@@ -49,56 +49,61 @@ class DashboardController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {   
+    {
         $profile_pic = User::join('images', 'users.id', '=', 'images.user_id')
-        ->where('users.id', Auth::user()->id)
-        ->get(['users.*', 'images.name as image_name']);
+            ->where('users.id', Auth::user()->id)
+            ->get(['users.*', 'images.name as image_name']);
 
-        $cstudent = User::where([ ['role', '=','Student'], ['status', '=','Active'] ])->count();
-        $cinstructor = User::where([ ['role', '=','Instructor'], ['status', '=','Active'] ] )->count();
+        $cstudent = User::where([['role', '=', 'Student'], ['status', '=', 'Active']])->count();
+        $cinstructor = User::where([['role', '=', 'Instructor'], ['status', '=', 'Active']])->count();
         $cstaff = User::where('role', 'Staff')->count();
 
         $cschool = School::all()->count();
         $cnew_student = User::where([
             ['role', '=', 'Student'],
             ['created_at', '>=', Carbon::now()->subDays(30)]
-        ])->count();  
+        ])->count();
 
-        $notifications = Notification::join('users', 'users.id', '=', 'notifications.user_id') 
-                        ->orderBy('notifications.created_at', 'desc')
-                        ->get(['users.*', 'notifications.*']);  
-                         
-        $student_courses = StudentCourse::all();  
- 
-        $classes = Classes::all(); 
+        $notifications = Notification::join('users', 'users.id', '=', 'notifications.user_id')
+            ->orderBy('notifications.created_at', 'desc')
+            ->get(['users.*', 'notifications.*']);
+
+        $student_courses = StudentCourse::all();
+
+        $classes = Classes::all();
         $schools = School::all();
         $courses = Course::all();
         $school_courses = SchoolCourse::all();
         $branches = Branch::all();
         $days = Day::all();
-        $fleets = Fleet::all();  
+        $fleets = Fleet::all();
 
 
         $instructor_evaluation = User::where([
-                ['role', '=', 'Instructor'],
-                ['status', '=', 'Active']
-            ])->get();
+            ['role', '=', 'Instructor'],
+            ['status', '=', 'Active']
+        ])->get();
 
-            
+
         $student_certification = User::join('student_course as sc', 'sc.student_id', '=', 'users.id')
-                                        ->leftJoin('fleet_schedules as fs', 'fs.student_id', '=', 'users.id')
-                                        ->orderBy('fs.created_at', 'desc') 
-                                        ->where('sc.status', '=', 'completed')
-                                        ->get(['sc.status as sc_status', 'fs.status as fs_status','users.*']);
-                                        
+            ->leftJoin('fleet_schedules as fs', 'fs.student_id', '=', 'users.id')
+            ->orderBy('fs.created_at', 'desc')
+            ->where('sc.status', '=', 'completed')
+            ->get(['sc.status as sc_status', 'fs.status as fs_status', 'users.*']);
+
         $student_access_practical = StudentCourse::where([
             ['status', '=', 'completed'],
             ['evaluation', '=', 'pass'],
             ['student_id', '=', Auth::user()->id],
-        ])->get(); 
-            
+        ])->get();
+
+
+        $student_info = Classes::select()->where("student_id", "=",  Auth::user()->id)->first();
+
+        // dd($student_info);
+
         // dd($student_certification);
-        return view('dashboard', compact('student_access_practical','instructor_evaluation', 'student_certification', 'cstudent', 'cschool','fleets', 'cstaff','cinstructor', 'cnew_student', 'notifications', 'profile_pic', 'branches', 'days', 'student_courses', 'classes', 'schools', 'courses', 'school_courses'));
+        return view('dashboard', compact('student_access_practical', 'student_info', 'instructor_evaluation', 'student_certification', 'cstudent', 'cschool', 'fleets', 'cstaff', 'cinstructor', 'cnew_student', 'notifications', 'profile_pic', 'branches', 'days', 'student_courses', 'classes', 'schools', 'courses', 'school_courses'));
     }
 
     /**
@@ -106,68 +111,69 @@ class DashboardController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
-     */ 
+     */
     public function store(Request $request)
     {
         //
     }
 
 
-    public function certificate($id){ 
+    public function certificate($id)
+    {
 
         $student_cert = User::join('student_course as sc', 'sc.student_id', '=', 'users.id')
-                            ->leftJoin('fleet_schedules as fs', 'fs.student_id', '=', 'users.id')  
-                            ->join('school_course as shc', 'shc.id', '=', 'sc.school_course_id') 
-                            ->join('courses as c', 'c.id', '=', 'shc.course_id') 
-                            ->leftJoin('images as i', 'i.user_id', '=', 'users.id')  
-                            ->where([
-                                ['users.id', '=', $id],
-                                ['sc.status', '=', 'completed'],
-                                ['fs.status', '=', 'completed'] 
-                            ])
-                            ->first(
-                                [
-                                    'i.name as image_name',
-                                    'sc.status as sc_status', 
-                                    'sc.civil_status', 
-                                    'sc.evaluation as sc_evaluation',
-                                    'c.name as c_name', 
-                                    'shc.start as shc_start',
-                                    'shc.end as shc_end',
-                                    'shc.duration as shc_duration',
-                                    'shc.id as shc_id',
-                                    'fs.start as fs_start',
-                                    'fs.end as fs_end',
-                                    'fs.duration as fs_duration',
-                                    'fs.status as fs_status',  
-                                    'fs.driver_license_no',  
-                                    'fs.control_no', 
-                                    'fs.date_issue',
-                                    'fs.evaluation as fs_evaluation',
-                                    'users.*'
-                                ]
-                            );
- 
-                            date_default_timezone_set('Asia/Manila');
-                            $age = Carbon::parse($student_cert->dob)->diff(Carbon::now())->y;
-                            $now = Carbon::now(); 
+            ->leftJoin('fleet_schedules as fs', 'fs.student_id', '=', 'users.id')
+            ->join('school_course as shc', 'shc.id', '=', 'sc.school_course_id')
+            ->join('courses as c', 'c.id', '=', 'shc.course_id')
+            ->leftJoin('images as i', 'i.user_id', '=', 'users.id')
+            ->where([
+                ['users.id', '=', $id],
+                ['sc.status', '=', 'completed'],
+                ['fs.status', '=', 'completed']
+            ])
+            ->first(
+                [
+                    'i.name as image_name',
+                    'sc.status as sc_status',
+                    'sc.civil_status',
+                    'sc.evaluation as sc_evaluation',
+                    'c.name as c_name',
+                    'shc.start as shc_start',
+                    'shc.end as shc_end',
+                    'shc.duration as shc_duration',
+                    'shc.id as shc_id',
+                    'fs.start as fs_start',
+                    'fs.end as fs_end',
+                    'fs.duration as fs_duration',
+                    'fs.status as fs_status',
+                    'fs.driver_license_no',
+                    'fs.control_no',
+                    'fs.date_issue',
+                    'fs.evaluation as fs_evaluation',
+                    'users.*'
+                ]
+            );
 
-                            $date_now =  $now->year . '-' . $now->month. '-' . $now->day;
+        date_default_timezone_set('Asia/Manila');
+        $age = Carbon::parse($student_cert->dob)->diff(Carbon::now())->y;
+        $now = Carbon::now();
 
-                            $ti = User::join('school_course as shc', 'shc.instructor_id', '=', 'users.id') 
-                                ->join('student_course as stc', 'stc.school_course_id', '=', 'shc.id')
-                                ->where('shc.id', '=', $student_cert->shc_id)
-                                ->first(['users.*']);
+        $date_now =  $now->year . '-' . $now->month . '-' . $now->day;
 
-                            $pi = User::join('fleet_schedules as fs', 'fs.instructor_id', '=', 'users.id') 
-                                    ->join('fleet as f', 'f.id', '=', 'fs.fleet_id') 
-                                    ->where('fs.student_id', '=', $id)
-                                    ->first(['users.*']);
+        $ti = User::join('school_course as shc', 'shc.instructor_id', '=', 'users.id')
+            ->join('student_course as stc', 'stc.school_course_id', '=', 'shc.id')
+            ->where('shc.id', '=', $student_cert->shc_id)
+            ->first(['users.*']);
 
- 
+        $pi = User::join('fleet_schedules as fs', 'fs.instructor_id', '=', 'users.id')
+            ->join('fleet as f', 'f.id', '=', 'fs.fleet_id')
+            ->where('fs.student_id', '=', $id)
+            ->first(['users.*']);
 
-         
-        return view('certificate', compact('student_cert', 'age', 'date_now', 'ti','pi'));
+
+
+
+        return view('certificate', compact('student_cert', 'age', 'date_now', 'ti', 'pi'));
     }
     /**
      * Display the specified resource.
@@ -175,40 +181,41 @@ class DashboardController extends Controller
      * @param  \App\Dashboard  $dashboard
      * @return \Illuminate\Http\Response
      */
-    public function showInstructorEvaluation($id){
-         
-        $instructor_evaluation = Evaluation::join('users as u', 'u.id', '=', 'evaluation.instructor_id') 
-                                ->where([
-                                    ['evaluation.instructor_id', '=', '$id'],
-                                    ['u.role', '=', 'Instructor']
-                                ])
-                                ->get(['evaluation.*']);
-                                 
+    public function showInstructorEvaluation($id)
+    {
+
+        $instructor_evaluation = Evaluation::join('users as u', 'u.id', '=', 'evaluation.instructor_id')
+            ->where([
+                ['evaluation.instructor_id', '=', '$id'],
+                ['u.role', '=', 'Instructor']
+            ])
+            ->get(['evaluation.*']);
+
         return view('admin/evaluation', compact('instructor_evaluation'));
     }
     public function show($username)
-    { 
+    {
 
-        
+
         $profile_pic = User::join('images', 'users.id', '=', 'images.user_id')
-                        ->where('users.id', Auth::user()->id)
-                        ->get(['users.*', 'images.name as image_name']); 
+            ->where('users.id', Auth::user()->id)
+            ->get(['users.*', 'images.name as image_name']);
 
-        $user = User::where('username', '=', $username)->first(); 
+        $user = User::where('username', '=', $username)->first();
 
 
-        $student_courses = StudentCourse::all();  
-        $classes = Classes::all(); 
+        $student_courses = StudentCourse::all();
+        $classes = Classes::all();
         $schools = School::all();
         $courses = Course::all();
         $school_courses = SchoolCourse::all();
         $branches = Branch::all();
-        $days = Day::all(); 
+        $days = Day::all();
 
 
-        
-        
-        return view('admin/student_schedule',compact( 'student_courses', 'classes', 'schools', 'courses', 'school_courses', 'branches', 'days', 'user', 'profile_pic'));
+
+
+        return view('admin/student_schedule', compact('student_courses', 'classes', 'schools', 'courses', 'school_courses', 'branches', 'days', 'user', 'profile_pic'));
     }
 
     /**
--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -1,10 +1,10 @@
 
-    <style> 
+    <style>
         #theoretical>a:hover, #theoretical>a{
             color:#38c172;
             text-decoration: none;
         }
-        
+
         #practical>a:hover, #practical>a{
             color: #e3342f;
             text-decoration: none;
@@ -14,49 +14,56 @@
             opacity: .3;
         }
   </style>
-<div class="page-header"> 
+<div class="page-header">
     @if(Auth::user()->role == 'Student')
         <a href="{{ route('student.scheduling') }}" class="btn btn-success btn-icon pull-right">
-            <i class="mdi mdi-calendar-text"></i> 
-            Scheduling 
+            <i class="mdi mdi-calendar-text"></i>
+            Scheduling
         </a>
     @endif
     <h3>Dashboard</h3>
-</div>  
+</div>
 
-<div class="row"> 
+<div class="row">
     <!-- stat 1 -->
     <div class="col-md-3">
         <div class="card widget" id="theoretical">
-            <a href="{{ route('schedule.theoretical')}}" > 
-                <div class="widget-icon widget-success">
+            <a href="{{ route('schedule.theoretical')}}" class="
+            @if(Auth::user()->role == 'Instructor' )
+                @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Theory")
+                @else disabled  @endif  @endif
+            ">
+                <div class="widget-icon @if(Auth::user()->role == 'Instructor' ) widget-success  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Theory")  widget-success @else bg-gray @endif @endif">
                     <i class="mdi mdi-blur-radial"></i>
                 </div>
-                <div class="widget-title">
+                <div class="@if(Auth::user()->role == 'Instructor' ) widget-title  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Theory")  widget-title  @else bg-gray @endif  @endif">
                     <h2>Theoretical</h2>
                     <p>School Schedule</p>
-                </div>  
+                </div>
             </a>
         </div>
     </div>
-    <!-- stat 2 -->  
- 
-    
+    <!-- stat 2 -->
+
+
         <div class="col-md-3">
-            <div class="card widget" id="practical"> 
-                <a href="{{ route('schedule.practical')}}" class="@if(Auth::user()->role == 'Instructor' ) @else @if(!$student_access_practical->isEmpty())  @else disabled  @endif  @endif"    > 
-                    <div class="widget-icon @if(Auth::user()->role == 'Instructor' ) widget-danger  @else @if(!$student_access_practical->isEmpty())  widget-danger @else bg-gray @endif @endif">
+            <div class="card widget" id="practical">
+                <a href="{{ route('schedule.practical')}}" class="
+                @if(Auth::user()->role == 'Instructor' )
+                @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Practical")
+                @else disabled  @endif  @endif
+                "    >
+                    <div class="widget-icon @if(Auth::user()->role == 'Instructor' ) widget-danger  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Practical")  widget-danger @else bg-gray @endif @endif">
                         <i class="mdi mdi-car-connected"></i>
                     </div>
-                    <div class=" @if(Auth::user()->role == 'Instructor' ) widget-title  @else @if(!$student_access_practical->isEmpty())  widget-title  @else bg-gray @endif  @endif ">
-                         
+                    <div class=" @if(Auth::user()->role == 'Instructor' ) widget-title  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Practical")  widget-title  @else bg-gray @endif  @endif ">
+
                         <h2>Practical</h2>
                         <p>Fleet Schedule</p>
-                    </div> 
+                    </div>
                 </a>
             </div>
-        </div>  
-  
+        </div>
+
 </div>
 
- 
