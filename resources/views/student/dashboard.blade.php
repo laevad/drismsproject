3af8f7dd398bf9afb03dac6772ca0d0319f9b618
laevad <djaldave@gmail.com>
2022-06-19 15:23:10 +0800

    unlocked registration inside

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -24,46 +24,48 @@
     <h3>Dashboard</h3>
 </div>
 
-<div class="row">
-    <!-- stat 1 -->
-    <div class="col-md-3">
-{{--        <div class="card widget" id="theoretical">--}}
-{{--            <a href="{{ route('schedule.theoretical')}}" class="--}}
-{{--            @if(Auth::user()->role == 'Instructor' )--}}
-{{--                @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Theory")--}}
-{{--                @else disabled  @endif  @endif--}}
-{{--            ">--}}
-{{--                <div class="widget-icon @if(Auth::user()->role == 'Instructor' ) widget-success  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Theory")  widget-success @else bg-gray @endif @endif">--}}
-{{--                    <i class="mdi mdi-blur-radial"></i>--}}
-{{--                </div>--}}
-{{--                <div class="@if(Auth::user()->role == 'Instructor' ) widget-title  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Theory")  widget-title  @else bg-gray @endif  @endif">--}}
-{{--                    <h2>Theoretical</h2>--}}
-{{--                    <p>School Schedule</p>--}}
-{{--                </div>--}}
-{{--            </a>--}}
-{{--        </div>--}}
-    </div>
-    <!-- stat 2 -->
+<div class="col-md-10 m-auto">
+    <div class="row ">
+        <!-- stat 1 -->
+        <div class="col-md-5">
+            <div class="card widget" id="theoretical">
+                <a href="{{ route('schedule.theoretical')}}" class="
+            @if(Auth::user()->role == 'Instructor' )
+                @else @if($student_access_practical->isEmpty())
+                @else disabled  @endif  @endif
+            ">
+                    <div class="widget-icon @if(Auth::user()->role == 'Instructor' ) widget-success  @else @if($student_access_practical->isEmpty())  widget-success @else bg-gray @endif @endif">
+                        <i class="mdi mdi-blur-radial"></i>
+                    </div>
+                    <div class="@if(Auth::user()->role == 'Instructor' ) widget-title  @else @if($student_access_practical->isEmpty())  widget-title  @else bg-gray @endif  @endif">
+                        <h2>Theoretical</h2>
+                        <p>School Schedule</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <!-- stat 2 -->
 
 
-        <div class="col-md-3">
-{{--            <div class="card widget" id="practical">--}}
-{{--                <a href="{{ route('schedule.practical')}}" class="--}}
-{{--                @if(Auth::user()->role == 'Instructor' )--}}
-{{--                @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Practical")--}}
-{{--                @else disabled  @endif  @endif--}}
-{{--                "    >--}}
-{{--                    <div class="widget-icon @if(Auth::user()->role == 'Instructor' ) widget-danger  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Practical")  widget-danger @else bg-gray @endif @endif">--}}
-{{--                        <i class="mdi mdi-car-connected"></i>--}}
-{{--                    </div>--}}
-{{--                    <div class=" @if(Auth::user()->role == 'Instructor' ) widget-title  @else @if(!$student_access_practical->isEmpty() || $student_info->type=="Practical")  widget-title  @else bg-gray @endif  @endif ">--}}
+        <div class="col-md-5">
+            <div class="card widget" id="practical">
+                <a href="{{ route('schedule.practical')}}" class="
+                @if(Auth::user()->role == 'Instructor' )
+                @else @if($student_access_practical->isEmpty())
+                @else disabled  @endif  @endif
+                "    >
+                    <div class="widget-icon @if(Auth::user()->role == 'Instructor' ) widget-danger  @else @if($student_access_practical->isEmpty())  widget-danger @else bg-gray @endif @endif">
+                        <i class="mdi mdi-car-connected"></i>
+                    </div>
+                    <div class=" @if(Auth::user()->role == 'Instructor' ) widget-title  @else @if($student_access_practical->isEmpty())  widget-title  @else bg-gray @endif  @endif ">
 
-{{--                        <h2>Practical</h2>--}}
-{{--                        <p>Fleet Schedule</p>--}}
-{{--                    </div>--}}
-{{--                </a>--}}
-{{--            </div>--}}
+                        <h2>Practical</h2>
+                        <p>Fleet Schedule</p>
+                    </div>
+                </a>
+            </div>
         </div>
 
+    </div>
 </div>
 
--- resources/views/student/schedule_practical.blade.php
+++ resources/views/student/schedule_practical.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.header') 
-@section('content')      
-    @include("layouts/includes/sidebar")     
+@extends('layouts.header')
+@section('content')
+    @include("layouts/includes/sidebar")
 
     <style>
 
@@ -8,7 +8,7 @@
             list-style: none;
         }
         .form-drisms tbody tr td{
-            padding-top: 0px !important; 
+            padding-top: 0px !important;
             padding-bottom: 0px !important
         }
         .form-drisms tbody tr{
@@ -17,76 +17,76 @@
     </style>
 
 
-<div class="main-content"> 
-    <div class="page-header"> 
+<div class="main-content">
+    <div class="page-header">
         <h3>Your Schedule for fleet</h3>
-    </div>  
+    </div>
     <!-- students growth -->
- 
+
     @if (\Session::has('success'))
         <div class="alert alert-success">
-            {!! \Session::get('success') !!} 
+            {!! \Session::get('success') !!}
         </div>
-    @endif   
+    @endif
     @if (\Session::has('error'))
         <div class="alert alert-danger">
-            {!! \Session::get('error') !!} 
+            {!! \Session::get('error') !!}
         </div>
-    @endif 
+    @endif
 
 
-    <div class="row">    
-        <div class="col-md-12">  
+    <div class="row">
+        <div class="col-md-12">
                 <table class="table table-striped">
                     <thead>
-                        <tr>  
+                        <tr>
                             <th scope="col">No. </th>
                             <th scope="col">Instructor</th>
                             <th scope="col">Make and Model</th>
                             <th scope="col">Model Year</th>
                             <th scope="col">Car Number</th>
                             <th scope="col">Car Plate</th>
-                            <th scope="col">Time Start & End</th> 
+                            <th scope="col">Time Start & End</th>
                             <th scope="col">Date Start</th>
                             <th scope="col">Date End</th>
                             <th scope="col">Days</th>
-                            <th scope="col">Duration & Period</th>                                                           
-                            <th scope="col">Action</th>                                                           
+                            <th scope="col">Duration & Period</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
-                    <tbody> 
-                        
+                    <tbody>
+
                         @forelse($fleet_schedules as $key => $val)
-                        
+
                             <form action="{{route('student.fleet.form.update', $val->id)}}" method="POST">
 
                                 @csrf
-                                @method('PUT') 
+                                @method('PUT')
                                 <input type="hidden" name="_student_id" value="{{ Auth::user()->id }}" />
-                                    <tr> 
-                                        <td> {{ $key + 1 }} </td> 
-                                        <td>{{ $val->fname }} {{ $val->lname }}</td> 
-                                        <td>{{ $val->make }} {{ $val->model }} </td>  
-                                        <td>{{ $val->model }}</td>  
-                                        <td>{{ $val->car_no }}</td>  
-                                        <td>{{ $val->car_plate }}</td>  
-                                        <td>{{ $val->time_start_end }}</td>  
-                                        <td>{{ $val->start }}</td> 
-                                        <td>{{ $val->end }}</td> 
-                                        <td>{{ $val->day }}</td> 
-                                        <td>{{ $val->duration }} {{ $val->period }}</td>  
-                                        <td>  <input type="submit" value="Register" class="btn btn-primary " @if(!empty($single_fleet)) disabled @endif> </td>  
-                                    </tr> 
-                            
+                                    <tr>
+                                        <td> {{ $key + 1 }} </td>
+                                        <td>{{ $val->fname }} {{ $val->lname }}</td>
+                                        <td>{{ $val->make }} {{ $val->model }} </td>
+                                        <td>{{ $val->model }}</td>
+                                        <td>{{ $val->car_no }}</td>
+                                        <td>{{ $val->car_plate }}</td>
+                                        <td>{{ $val->time_start_end }}</td>
+                                        <td>{{ $val->start }}</td>
+                                        <td>{{ $val->end }}</td>
+                                        <td>{{ $val->day }}</td>
+                                        <td>{{ $val->duration }} {{ $val->period }}</td>
+                                        <td>  <input type="submit" value="Register" class="btn btn-primary " @if(empty($single_fleet)) disabled @endif> </td>
+                                    </tr>
+
                             </form>
                         @empty
-                        @endforelse 
+                        @endforelse
                     <tbody>
-                </table> 
+                </table>
 
 
         </div>
-    </div> 
+    </div>
 
 
 
@@ -95,8 +95,8 @@
 </div>
 
 
-@include('../layouts/includes/footer')  
+@include('../layouts/includes/footer')
 
-<script> 
+<script>
 </script>
 @endsection
